7acf59698078072f7514c5a153c530bff08aab89
abdullah107189 <abdullah107189@gmail.com>
2025-05-07 22:33:52 +0600

    problem solve 2

--- solution.ts
+++ solution.ts
@@ -7,3 +7,19 @@ function formatString(input: string, toUpper?: boolean): string {
 }
 const problem1 = formatString("Hello", true);
 // console.log(problem1);
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  const arrayFilter = items.filter((item) => item.rating >= 4);
+  return arrayFilter;
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+const problem2 = filterByRating(books);
+// console.log(problem2);
