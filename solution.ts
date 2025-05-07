645e359f6f72d40be993cb9222099a73dd8fcea2
abdullah107189 <abdullah107189@gmail.com>
2025-05-07 22:33:04 +0600

    problem solve 1

--- solution.ts
+++ solution.ts
@@ -1,2 +1,9 @@
-const names: string = "hello";
-console.log(names);
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === true) {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
+const problem1 = formatString("Hello", true);
+// console.log(problem1);
