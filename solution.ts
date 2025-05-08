332d5eb5f997a547be2ae9a437f1940fea69cf8a
abdullah107189 <abdullah107189@gmail.com>
2025-05-08 11:24:47 +0600

    5 number problem solved

--- solution.ts
+++ solution.ts
@@ -51,3 +51,13 @@ class Car extends Vehicle {
 const myCar = new Car("Toyota", 2020, "Corolla");
 // console.log(myCar.getInfo());
 // console.log(myCar.getModel());
+
+function processValue(value: string | number): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+// console.log(processValue(10));
+
